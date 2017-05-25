05a28489c241efd1dd3724ca2743549417f3fd53
Jason Brodeur <jason.brodeur@gmail.com>
2017-05-25 16:17:22 -0400

    updates

--- georef_rewarp.m
+++ georef_rewarp.m
@@ -1,4 +1,4 @@
-function [logfile] = georef_rewarp(series_label, georef_list)
+function [logfile] = georef_rewarp(series_label, georef_list, clipping_flag)
 % georef_rewarp.m
 % This function runs through a collection of maps sheets (whether 1:25000 or 1:63360, as specified by series_label),
 % looks for corresponding gcp files--and where they exist, performs georeferencing and georectification.
@@ -7,15 +7,25 @@ function [logfile] = georef_rewarp(series_label, georef_list)
 % georef_list: file name of a single column list of filenames for sheets to be processed (optional); file must exist in the master_path directory 
 % (i.e. /AutoGeoref/1_25000/ or /AutoGeoref/1_63360/ 
 % where georef_list is not provided, the function works through the entire 
+% clipping_flag indicates whether or not a clipped (to the neatline) version should be created with the unclipped one. The clipped image is saved to /geotiff_clipped
+%%%clipping_flag = 0 indicates no clipped image to be created, while clippeing_flag=1 causes a clipped image to be produced.
 
 if nargin == 0
     disp(['The variable ''series_label'' needs to be set to ''63360'' or ''25000''. Exiting.']);
     break
 elseif nargin == 1
     dir_flag = 1 % if only one argument (series label) is provided, then run through the entire /tif directory
-else
+    clipping_flag = 0;
+elseif nargin ==2
     dir_flag = 0; % if a list is provided, the function will run through all filenames provided in the list.
-    end
+    clipping_flag = 0;
+else
+    dir_flag = 0;
+end
+
+if isempty(georef_list)==1
+dir_flag = 1;
+end
 
 %% Variables
 % variables -- these could be set in a function call (since s_srs and t_srs may not be constant).
@@ -69,14 +79,21 @@ switch series_label
     disp(['The variable ''series_label'' needs to be set to ''63360'' or ''25000''. Exiting.']);
     break
 end
+geotiff_clipped_path = [master_path 'geotiff_clipped'];
     t_srs_tag = t_srs;
-
+    
 %% Make the output folders (if necessary):
 for k = 1:1:length(t_srs)
   if exist([geotiff_path t_srs{k}])~=7
     if ispc==1; [status,cmdout] = dos(['mkdir "' geotiff_path t_srs{k} '"']); else [status,cmdout] = unix(['mkdir "' geotiff_path t_srs{k} '"']);end
       if status==0
       disp(['Created directory: ' geotiff_path t_srs{k}]);
+      end
+  end
+  if exist([geotiff_clipped_path t_srs{k}])~=7
+    if ispc==1; [status,cmdout] = dos(['mkdir "' geotiff_clipped_path t_srs{k} '"']); else [status,cmdout] = unix(['mkdir "' geotiff_clipped_path t_srs{k} '"']);end
+      if status==0
+      disp(['Created directory: ' geotiff_clipped_path t_srs{k}]);
       end
   end
 end
@@ -92,6 +109,19 @@ epsg_lookup(:,2) = tmp{1,2}(:,1);
 epsg_lookup(:,3) = tmp{1,3}(:,1);
 fclose(fid_srs);
 %end
+clear tmp;
+
+%%% Load in the NTS lookup table (to get extents for the sheet, to be used in clipping).
+% Sheet | East | West | South | North
+fid_nts = fopen([top_path 'NTS_TopoCorners_1_' series_label '.csv'],'r');
+tmp = textscan(fid_nts,'%s %f %f %f %f','Delimiter',',','headerlines',1);
+nts_lookup(:,1) = tmp{1,1}(:,1); % sheet
+nts_lookup(:,2) = tmp{1,2}(:,1); % east
+nts_lookup(:,3) = tmp{1,3}(:,1); % west
+nts_lookup(:,4) = tmp{1,4}(:,1); % south
+nts_lookup(:,5) = tmp{1,5}(:,1); % north
+fclose(fid_nts);
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% get the directory listing in /tif; pare down to a list of only tif files:
@@ -143,6 +173,14 @@ for i = 1:1:length(d)
     filename_in = d(i).name;
     [fdir, fname, fext] = fileparts(filename_in); %file directory | filename | file extension
     logfile{i,1} = filename_in;
+    
+    % Attempt to pull out the year of publication from the filename (should be the first four characters after the first "_")
+    uscores = strfind(fname,'_');
+    pubyear_str = fname(uscores(1)+1:uscores(1)+4);
+    disp(['Publication year is ' pubyear_str ]);
+    pubyear = str2double(pubyear_str);
+    sheetname = fname(1:uscores(1)-1);
+    
     % look for the corresponding GCP file in the gcp directory (named the same but with .txt extension)
     if exist([gcp_path fname '.txt'],'file')==2
     disp(['Now working on file: ' filename_in]);
@@ -190,6 +228,15 @@ for i = 1:1:length(d)
             continue
         end
         
+        %%%% For 1:50000 sheets only -- modify the s_srs to the NAD83 srs if the sheet pubyear is post-1984 AND we're using a UTM projection.
+        switch series_label
+          case '50000'
+          if length(s_srs)==5 & strncmp(s_srs,'26',2)==1 & pubyear > 1984
+          disp(['modifying source srs from ' s_srs ' to ' num2str(str2double(s_srs)+200)]);
+          s_srs = num2str(str2double(s_srs)+200);
+          end
+        end
+        
 %        if exist([gcp_path fname '.txt'],'file')~=2
 %            disp(['Could not find the gcp file for ' fname '. Skipping.']);
 %            logfile{i,2} = 'gcp_file';
@@ -206,11 +253,17 @@ for i = 1:1:length(d)
         if size(C,1)>6; trans_order = '2'; else trans_order = '1'; end
         
         % format of C:
-        % x (inches right) | y (inches up) | x_map (long) | y_map (lat)
+        % x (inches right) | y (inches up) | x_map (lng) | y_map (lat)
         x = C(:,1);
         y = C(:,2);
-        long = C(:,3);
+        lng = C(:,3);
         lat = C(:,4);
+        
+%        % Extract the four extents from the long and lat category (we'll use this for clipping the image to the neatline)
+%        % We've also made an assumption here that at least one GCP exists on each of the margins.
+%        lng_min = num2str(min(lng)); lng_max = num2str(max(lng));
+%        lat_min = num2str(min(lat)); lat_max = num2str(max(lat));
+%        
         
         %Create the qgis .points file:
         fid_qgis = fopen([qgis_gcp_path filename_in '.points'],'w');
@@ -218,16 +271,38 @@ for i = 1:1:length(d)
         fclose(fid_qgis);
         
         %%% Create format for qgis file:
-        C_QGIS = [long lat x.*ppi_in (y.*ppi_in)-h ones(length(x),1)];
+        C_QGIS = [lng lat x.*ppi_in (y.*ppi_in)-h ones(length(x),1)];
         dlmwrite([qgis_gcp_path filename_in '.points'],C_QGIS,"-append");
+        
+       %% Extract the extents of the map in the four cardinal directions. We need this in lat/long, so we'll need to find it in the lookup table.
+       right_row = find(strcmp(sheetname,nts_lookup(:,1))==1);
+       lng_max = nts_lookup{right_row,2};
+       lng_min = nts_lookup{right_row,3};
+       lat_min = nts_lookup{right_row,4};
+       lat_max = nts_lookup{right_row,5};
+       
+        switch fname(end)
+          case 'E'
+          lng_min = lng_min + (lng_max - lng_min)./2;
+          case 'W'
+          lng_max = lng_max - (lng_max - lng_min)./2;
+          otherwise
+        end
+        
+        % Set the coordinate system for clipping (according to publication year)
+        if pubyear > 1984 
+        te_srs = 4269;
+        else
+        te_srs = 4267;
+        end
         
         %%% Generate gdal_translate string
         % ratio of ppi_out to ppi_in
         out_ratio = ppi_out./ppi_in;
         out_pct = round(out_ratio*10000)./100;
         disp(['out_ratio is: ' num2str(out_ratio) '. out_pct = ' num2str(out_pct)]);
-%        C_GDAL = [x*ppi_out (h./2)-(y*ppi_out) long lat];
-        C_GDAL = [x*ppi_out (h.*out_ratio)-(y*ppi_out) long lat];
+%        C_GDAL = [x*ppi_out (h./2)-(y*ppi_out) lng lat];
+        C_GDAL = [x*ppi_out (h.*out_ratio)-(y*ppi_out) lng lat];
         gdal_str = '';
         for j = 1:1:size(C_GDAL,1)
             gdal_str = [gdal_str '-gcp ' num2str(C_GDAL(j,1),8) ' ' num2str(C_GDAL(j,2),8) ' ' num2str(C_GDAL(j,3),8) ' ' num2str(C_GDAL(j,4),8) ' '];
@@ -271,6 +346,28 @@ for i = 1:1:length(d)
           end
           disp(['Transformation of ' filename_in ' was successful.']);
           logfile{i,2} = 'clear!';
+          
+          %%% if clipping_flag==1, run gdalwarp command again, but clip to the neatline.
+          if clipping_flag==1
+          gdalwarp_cmd2 = ['gdalwarp -overwrite -q -r cubicspline -s_srs EPSG:' s_srs ' -t_srs EPSG:' t_srs{k} ' -te ' lng_min ' ' lat_min ' ' lng_max ' ' lat_max ' -te_srs EPSG:' te_srs ' -order ' trans_order ' -co COMPRESS=NONE -dstalpha "' master_path 'tmp.tif" "'...
+              geotiff_clipped_path t_srs_tag{k} '/' filename_in '"'];
+          disp(['Running gdalwarp (clipped) on ' filename_in '.']);
+          
+          if ispc==1; 
+          gdalwarp_cmd2 = ['C:\OSGeo4W64\bin\' gdalwarp_cmd2];
+          [status_warp2, msg_warp2] = dos(gdalwarp_cmd2); 
+          else [status_warp] = unix(gdalwarp_cmd2);
+          end
+          
+          if status_warp2~=0
+              disp(['gdalwarp (clipped) failed for: ' filename_in '. Skipping.']);
+              %logfile{i,2} = 'gdalwarp';
+              continue
+          end
+          
+
+          end
+          
         end
         
     else
